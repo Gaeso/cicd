f53aeedcff7c5445beb63cede19d655ca778bd11
gaeso <ghwns2239@gmail.com>
2025-07-29 14:56:43 +0900

    test

--- src/main/java/com/example/cicd/TestController.java
+++ src/main/java/com/example/cicd/TestController.java
@@ -1,4 +1,13 @@
 package com.example.cicd;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+	@GetMapping("/hi")
+	public String testMethod() {
+		return "내가 만든 서버";
+	}
 }
